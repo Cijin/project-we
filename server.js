7aacdf28e46f5a603b0718514fc25baf92760c68
cijin <cijininurhood@gmail.com>
2020-12-07 20:30:13 +0700

    socket works, video too. Quite a lot of progress made.

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
 
 const PORT = process.env.PORT || 3090;
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
+app.use("/peerjs", peerServer);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -15,13 +20,20 @@ app.get("/", (request, response) => {
 });
 
 app.get("/:room", (request, response) => {
-  res.render("room", { roomId: req.params.room });
+  response.render("room", { roomId: request.params.room });
 });
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
-    console.log(roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
   });
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log("server listening on: ", PORT);
+});
