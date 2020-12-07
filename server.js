9486798bb1eb727e505519c656cd10566b779162
cijin <cijininurhood@gmail.com>
2020-12-07 09:29:18 +0700

    using tutorial with ejs to create app

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require("express");
-const { ExpressPeerServer } = require("peer");
 const path = require("path");
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 80;
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
+const PORT = process.env.PORT || 3090;
+
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+app.get("/", (request, response) => {
+  response.redirect(`/${uuidV4()}`);
 });
 
-const listener = app.listen(PORT, () => {
-  console.log("Your app is listening on Port: ", PORT);
+app.get("/:room", (request, response) => {
+  res.render("room", { roomId: req.params.room });
 });
 
-const peerServer = ExpressPeerServer(listener, {
-  debug: true,
-  path: "/myapp",
-});
-
-app.use("/peerjs", peerServer);
+server.listen(PORT);
