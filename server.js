33c99b6e3940f11634a59e941ffede3106731866
cijin <cijininurhood@gmail.com>
2020-12-07 09:00:51 +0700

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { ExpressPeerServer } = require("peer");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static("public"));
 
